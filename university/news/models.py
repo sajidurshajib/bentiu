d4d53c1d61f6adcabae732aa1104b4238f6fab34
sajidurshajib <sajidur.inbox@gmail.com>
2024-05-19 00:24:21 +0600

    change: webp to jpg feature

--- university/news/models.py
+++ university/news/models.py
@@ -10,7 +10,7 @@ from tinymce.models import HTMLField
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
-def optimize_and_convert_to_webp(image):
+def optimize_and_convert_to_jpg(image):
     img = Image.open(image)
     # Compress the image to be less than 1MB
     max_size = 1024 * 1024  
@@ -18,18 +18,18 @@ def optimize_and_convert_to_webp(image):
     buffer = BytesIO()
     while True:
         buffer = BytesIO()
-        img.save(buffer, format='WEBP', quality=quality)
+        img.save(buffer, format='jpg', quality=quality)
         if buffer.tell() <= max_size or quality <= 10:
             break
         quality -= 5
         buffer.seek(0)
         img = Image.open(buffer)
-    return InMemoryUploadedFile(buffer, None, f"{image.name.split('.')[0]}.webp", 'image/webp', buffer.getbuffer().nbytes, None)
+    return InMemoryUploadedFile(buffer, None, f"{image.name.split('.')[0]}.jpg", 'image/jpg', buffer.getbuffer().nbytes, None)
 
 def generate_filename(instance, filename):
     current_time = datetime.now().strftime("%Y%m%d%H%M%S")
     slug = instance.slug
-    filename = f"{slug}_{current_time}.webp"
+    filename = f"{slug}_{current_time}.jpg"
     return os.path.join('news', filename)
 
 class News(models.Model):
@@ -45,7 +45,7 @@ class News(models.Model):
         if not self.id:
             self.slug = slugify(self.title)
         if self.cover:
-            self.cover = optimize_and_convert_to_webp(self.cover)
+            self.cover = optimize_and_convert_to_jpg(self.cover)
         super().save(*args, **kwargs)
 
     def __str__(self):
